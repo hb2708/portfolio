fdc326efcab938261ad824b7e130445eab2a7ba9
Harshal Bhavsar <hb2708@gmail.com>
2025-11-22 18:11:48 +0800

    feat: add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
@@ -16,6 +17,7 @@ function App() {
       <Experience />
       <Writing />
       <Contact />
+      <Analytics />
     </main>
   );
 }
